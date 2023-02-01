ea8503289eab0231419f008daa188c980cb22961
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2023-02-01 23:32:09 +0600

    s

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('Done').status(200)
+})
 app.post('/webhook', (req, res) => {
-  const message = req.body.entry[0].messaging[0].message.text;
+    const message = req.body.entry[0].messaging[0].message.text;
 
-  if (message.includes('hi')) {
-    // const client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
-    // client.messages.create({
-    //   to: 'YOUR_PHONE_NUMBER',
-    //   from: 'TWILIO_PHONE_NUMBER',
-    //   body: 'Incoming message includes the specific word'
-    // });
-    
-    console.log("Asche");
-  }
+    if (message.includes('hi')) {
+        // const client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+        // client.messages.create({
+        //   to: 'YOUR_PHONE_NUMBER',
+        //   from: 'TWILIO_PHONE_NUMBER',
+        //   body: 'Incoming message includes the specific word'
+        // });
 
-  res.sendStatus(200);
+        console.log("Asche");
+    }
+
+    res.sendStatus(200);
 });
 
 app.listen(3000, () => {
-  console.log('Chatbot server listening on port 3000');
+    console.log('Chatbot server listening on port 3000');
 });
